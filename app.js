116dd5093b5f76507a9950e9fb0e48f98c4331a6
NoodlesJS <romaenwritescode@gmail.com>
2019-11-05 23:06:33 -0600

    finished landing page

---
+++ app.js
@@ -0,0 +1,8 @@
+// burger menu interaction
+let burgerIcon = document.querySelector('.burger');
+
+burgerIcon.addEventListener('click', function() {
+    document.querySelector('.burger-menu').classList.toggle('hide');
+    // document.querySelector('.burger-menu').classList.toggle(show);
+    
+})
