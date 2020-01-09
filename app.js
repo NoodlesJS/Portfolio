ff19a476c567afa5d318d4255e3e33f0535d94a0
NoodlesJS <romaenwritescode@gmail.com>
2020-01-09 00:05:52 -0600

    added lop ui

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 // Collection of my works
 let Mywork = [
     {
+        name: 'LOP Network UI',
+        desc: 'UI design for clients application',
+        tech:'Figma',
+        button1: 'PREVIEW',
+        button2: 'NULL',
+        button1_style: 'button-filled',
+        button2_style: 'button-inactive',
+        button1_link: 'https://www.behance.net/gallery/90415571/LOP-network-UI',
+        button2_link: '#',
+        img: './img/projects/lopnetwork.png',
+        alt: 'Picture of project'
+    },
+    {
         name: 'Portfolio',
         desc: 'Personal Portfolio that was designed and built from scratch',
         tech:'Figma, HTML, CSS, Vanilla JS',
-        preview: 'https://noodlesjs.dev/',
-        code: 'https://github.com/NoodlesJS/Portfolio',
+        button1: 'PREVIEW',
+        button2: 'CODE',
+        button1_style: 'button-filled',
+        button2_style: 'button-hollow',
+        button1_link: 'https://noodlesjs.dev/',
+        button2_link: 'https://github.com/NoodlesJS/Portfolio',
         img: './img/projects/portfolio.png',
         alt: 'Picture of project'
     },
@@ -13,8 +30,12 @@ let Mywork = [
         name: 'Quick Pokedex',
         desc: 'A pokedex application that uses various web api to fetch quick short info about any pokemon from the offical pokedex',
         tech:'Figma, HTML, CSS, Vanilla JS',
-        preview: 'https://quickpokedex.netlify.com/',
-        code: 'https://github.com/NoodlesJS/Quick-Pokedex',
+        button1: 'PREVIEW',
+        button2: 'CODE',
+        button1_style: 'button-filled',
+        button2_style: 'button-hollow',
+        button1_link: 'https://quickpokedex.netlify.com/',
+        button2_link: 'https://github.com/NoodlesJS/Quick-Pokedex',
         img: './img/projects/quick_pokedex.png',
         alt: 'Picture of project'
     },
@@ -22,8 +43,12 @@ let Mywork = [
         name: 'USBD Market Access',
         desc: 'A landing page for the company USBD market access.',
         tech:'Figma, HTML, CSS',
-        preview: 'https://usbdmarketaccess.com/',
-        code: 'https://github.com/NoodlesJS/USBD-MARKET-ACCESS-Landing-page',
+        button1: 'PREVIEW',
+        button2: 'CODE',
+        button1_style: 'button-filled',
+        button2_style: 'button-hollow',
+        button1_link: 'https://usbdmarketaccess.com/',
+        button2_link: 'https://github.com/NoodlesJS/USBD-MARKET-ACCESS-Landing-page',
         img: './img/projects/usbd.png',
         alt: 'Picture of project'
     },
@@ -31,8 +56,12 @@ let Mywork = [
         name: 'Overwatch Quiz',
         desc: 'A quiz application based on the 1st Overwatch League season. This was the 1st application I ever built',
         tech:'HTML, CSS, Vanilla JS',
-        preview: 'https://noodlesjs.github.io/Overwatch-league-quiz/',
-        code: 'https://github.com/NoodlesJS/Overwatch-league-quiz',
+        button1: 'PREVIEW',
+        button2: 'CODE',
+        button1_style: 'button-filled',
+        button2_style: 'button-hollow',
+        button1_link: 'https://noodlesjs.github.io/Overwatch-league-quiz/',
+        button2_link: 'https://github.com/NoodlesJS/Overwatch-league-quiz',
         img: './img/projects/overwatch_quiz.png',
         alt: 'Picture of project'
     },
@@ -73,8 +102,8 @@ function loopWork() {
                     <p class="small-text">${project.desc}</p>
                     <p class="small-text"><i> Tech Used: ${project.tech}</i></p>
                     <div class="button-container">
-                        <a href="${project.preview}" target="_blank"><button class="button-filled">PREVIEW</button></a>
-                        <a href="${project.code}" target="_blank"><button class="button-hollow">CODE</button></a>
+                        <a href="${project.button1_link}" target="_blank"><button class="${project.button1_style}">${project.button1}</button></a>
+                        <a href="${project.button2_link}" target="_blank"><button class="${project.button2_style}">${project.button2}</button></a>
                     </div>
                 </div>
             </div>
